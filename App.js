8b1437b4ac633b16a917cbf53ae01247777c3309
Faith Tarbert <faithchendrickson@gmail.com>
2021-04-23 16:02:54 -0700

    more styling and start timers

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Platform } from 'react-native';
 import { Focus } from './src/features/focus/Focus';
+import { Timer } from './src/features/timer/Timer';
 import { colors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusSubject, setFocusSubject] = useState('gardening');
 
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Text>Here is where I am going to build a Timer</Text>
+        <Timer focusSubject={focusSubject}/>
       ) : (
         <Focus addSubject={setFocusSubject} />
       )}
@@ -21,6 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
     backgroundColor: colors.darkBlue,
   },
 });
+
